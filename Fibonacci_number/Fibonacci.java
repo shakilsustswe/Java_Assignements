d583974d974146b0593213d8b83065e1f8b4002d
ASHIQUL HASAN SHAKIL <53114821+shakilsustswe@users.noreply.github.com>
2020-03-01 01:31:47 +0600

    Add files via upload

---
+++ Fibonacci_number/Fibonacci.java
@@ -0,0 +1,25 @@
+
+package Fibonacci_number;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+ 
+    public static void main(String[] args)
+    {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter number:");
+        int n= input.nextInt();
+        int first=0;
+        int second=1;
+        int fibo;
+        System.out.print(first+" "+second);
+        for(int i=3;i<=n;i++)
+        {
+            fibo=first+second;
+            System.out.print(" "+fibo);
+            first=second;
+            second=fibo;
+        }
+    }
+}
